6eda10bc797389138ccbcad24c1ff0828e5ebc29
ilker Cantekin <454545ilker@gmail.com>
2023-02-01 02:41:32 +0300

    31ocakkk

--- src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
+++ src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
@@ -4,6 +4,7 @@ import baseURL.JsonPlaceHolerBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.delete;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
@@ -79,7 +80,7 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolerBaseUrl {
                 spec(specJsonPlace).
                 when().
                 get("/{pp1}/{pp2}");
-
+        response.prettyPrint();
         // 4 - Assertion
 
         response.
@@ -95,6 +96,25 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolerBaseUrl {
             request gonderdigimizde donen response’un status code’unun 200 oldugunu ve
             response body’sinin null oldugunu test edin
          */
+        // 1 - URL hazirla
 
+        specJsonPlace.pathParams("pp1","posts","pp2",50);
+
+        // 2 - Expected Data hazirla
+
+        // 3 - Response'i kaydet
+
+        Response response = given().
+                spec(specJsonPlace).
+                when().
+                delete("/{pp1}/{pp2}");
+        response.prettyPrint();
+
+        // 4 - Assertion
+
+        response.
+                then().
+                assertThat().statusCode(200).
+                body("title",equalTo(null));
     }
 }
